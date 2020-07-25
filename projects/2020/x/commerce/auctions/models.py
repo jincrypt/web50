c7ac959ba5b549858f85143d36c3632d9d570b06
Angus Leung <leung.mka@gmail.com>
2020-07-24 21:52:09 -0400

    Started models, incomplete

--- projects/2020/x/commerce/auctions/models.py
+++ projects/2020/x/commerce/auctions/models.py
@@ -1,6 +1,22 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.a`u`th.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    comment = 
+    pass
+
+class Listings(models.Model):
+    pass
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    bid
+    image = models.ImageField()
+
+class Bids():
+    pass
+
+class Comments():
+    listing = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name=)
+
     pass
