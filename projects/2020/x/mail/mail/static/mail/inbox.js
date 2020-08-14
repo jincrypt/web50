0d2c1104b987a945950df8e18bbae22fe1426322
Jincrypt <leung.mka@gmail.com>
2020-08-13 23:57:47 -0400

    Completed Mailbox Specifications

--- projects/2020/x/mail/mail/static/mail/inbox.js
+++ projects/2020/x/mail/mail/static/mail/inbox.js
@@ -50,12 +50,13 @@ function compose_email() {
           alert(data['error'])
         })
       }
-    })
+    });
     
     // Prevents default submit action
     return false;
   }
 }
+
 
 function load_mailbox(mailbox) {
   
@@ -65,4 +66,66 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  create_table(mailbox)
+}
+
+
+function create_table(mailbox) {
+
+  fetch(`/emails/${mailbox}`)
+  .then(response => {
+    response.json().then(data => {
+      // Creation of Table + Headers
+      const email_view = document.querySelector('#emails-view');
+      const table = document.createElement('table');
+      const who = (mailbox === 'sent') ? 'To' : 'From';
+      table.className = 'table table-hover table-sm';
+
+      table.innerHTML = `
+                        <thead class="thead-dark">
+                          <tr>
+                            <th style="width: 25%" scope="col">${who}</th>
+                            <th style="width: 55%" scope="col">Subject</th>
+                            <th style="width: 20%" scope="col">Date</th>
+                          </tr>
+                        </thead>`;
+
+      email_view.append(table);
+      // End of Table Creation
+
+      for (let i=0; i < data.length; i++) {
+        const new_row = document.createElement('tbody');
+        const address = (mailbox === 'sent') ? data[i].recipients : data[i].sender;
+        const subject = (data[i].subject.length > 70) ? data[i].subject.slice(0, 67) + ' ...' : data[i].subject;
+        const time = data[i].timestamp;
+        const id = data[i].id;
+
+
+        // Read background to grey as per specifications
+        if (data[i].read === true) {
+          new_row.className = 'table-active';
+        }
+
+        new_row.innerHTML = `<tr>
+                              <th scope="col">${address}</th>
+                              <th scope="col">${subject}</th>
+                              <th scope="col">${time}</th>
+                            </tr>`;
+
+        new_row.addEventListener('click', function() {
+          fetch(`/emails/${id}`)
+          .then(response => response.json().then(email => {
+            console.log(email);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~TODO */
+            // Insert Function for something
+          }))
+        })
+
+        // Insert email into table
+        table.insertBefore(new_row,null);
+
+      }
+    });
+  });
 }
